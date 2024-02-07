e1698a74d1e6040d135cf2cb0fa58784eb28147d
mhutch24 <Marsettbushell@yahoo.com>
2024-02-07 10:22:57 -0500

    test

---
+++ FocusHopeAnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  FocusHopeAnnotationView.swift
+//  FHMap
+//
+//  Created by Marsett Hutchons on 2/7/24.
+//
+
+import SwiftUI
+
+struct FocusHopeAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusHopeAnnotationView()
+}
